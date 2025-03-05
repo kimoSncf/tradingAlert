bf4765d25e7eff6cc67050d345db91d1d8689d88
kimoSncf <125282753+kimoSncf@users.noreply.github.com>
2025-03-05 13:56:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ pairs_data = {}
 
 # Configuration Binance
 exchange = ccxt.binance({
-    'apiKey': 'Q64QJuS4Ro048O5ucBqFCInkfxn8QaGJzkKZD7O8b3g2UFvzDsI3nhKCZMXXCQkJ',
-    'secret': 'YV8KJyOm2DTA5rUmVlVl7inmLwmd3uLQr3lnBmbaP463ia5CoWgxSbA2EU6EyHfn',
+    'apiKey': 'Q64QJuS4Ro048O5ucBqFCInFvzDsI3nhKCZMXXCQkJ',
+    'secret': 'YV8KJyOm2DTA5rmbaP463ia5CoWgxSbA2EU6EyHfn',
     'enableRateLimit': True
 })
 
